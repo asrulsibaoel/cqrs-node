2a55ace43353fd225ad06f624fe7a8e596e25da1
asrulsibaoel <asrulsibaoel@gmail.com>
2017-08-14 20:19:28 +0700

    updated: restify initialization

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const restify = require('restify');
+const clc = require('cli-color');
 import {EventStore, Container} from 'node-cqrs';
 import MongoEventStorage from 'node-cqrs-mongo';
-import database from 'config/database'
+import database from 'config/database';
 
 const container = new Container();
+
+const server = restify.createServer({
+    name: 'CQRS Node'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(CookieParser.parse);
+
+restify.CORS.ALLOW_HEADERS.push('content-type');
+restify.CORS.ALLOW_HEADERS.push('access-key');
+server.pre(restify.CORS({'origins': ['*']}));
 
 container.registerInstance({
     connectionString: 'mongodb://' + database.dbConfig.host + ':' + database.dbConfig.port + '/' + database.dbConfig.dbName
@@ -10,3 +25,9 @@ container.registerInstance({
 container.register(MongoEventStorage, 'storage');
 container.register(EventStore, 'eventStore');
 
+
+require('./src/http/Router');
+const port = (process.env.PORT) ? process.env.PORT : 8030;
+server.listen(port, () => {
+    console.log("---[============]---\n" + clc.bgYellowBright(server.name + ' Listening at port: ' + port) + "\n---[============]---");
+});
